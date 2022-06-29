6fe02a7e497bae7adce5e94a740e62cf99f80f78
AIRTON <AIRTON@192.168.1.19>
2022-06-29 17:13:08 -0500

    querydeairton

--- src/main/java/pe/edu/upc/demo/repositories/IMarcaRepository.java
+++ src/main/java/pe/edu/upc/demo/repositories/IMarcaRepository.java
@@ -1,11 +1,22 @@
 package pe.edu.upc.demo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.demo.entities.Marca;
+
 @Repository
-public interface IMarcaRepository extends JpaRepository<Marca,Integer>{
-	
+public interface IMarcaRepository extends JpaRepository<Marca, Integer> {
+
+	@Query(value = "Select m.nombre_marca, count(dtc.id_talla_producto) as VentasdeMarcas\r\n"
+			+ "from marca m join producto p on m.id_marca= p.id_marca \r\n"
+			+ "inner join talla_producto t on t.id_producto=p.id_producto\r\n"
+			+ "inner join detallecompra dtc on dtc.id_talla_producto= t.id_talla_producto\r\n"
+			+ "inner join compra c on c.id_compra=dtc.id_compra\r\n"
+			+ "inner join usuario u on u.id_usuario=c.id_usuario \r\n" + "group by m.id_marca", nativeQuery = true)
+	public List<String[]> ventasMarca();
 
 }
--- src/main/java/pe/edu/upc/demo/serviceinterface/IMarcaService.java
+++ src/main/java/pe/edu/upc/demo/serviceinterface/IMarcaService.java
@@ -10,11 +10,13 @@ public interface IMarcaService {
 	public void Insert(Marca marca);
 
 	public List<Marca> list();
-	
+
 	public void delete(int idMarca);
-	
+
 	Optional<Marca> listId(int idMarca);
-	
+
 	public void update(Marca marca);
+
+	public List<String[]> ventasMarca();
 
 }
