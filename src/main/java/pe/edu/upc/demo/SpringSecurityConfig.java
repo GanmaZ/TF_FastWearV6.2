413622859747dfd7d02a2a06487c56df13c2f542
GanmaZ <Jimegch@hotmail.com>
2022-08-26 21:52:29 -0500

    upgr

--- src/main/java/pe/edu/upc/demo/SpringSecurityConfig.java
+++ src/main/java/pe/edu/upc/demo/SpringSecurityConfig.java
@@ -25,7 +25,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.authorizeRequests().antMatchers("/","/css/**", "/js/**", "/images/**","/usuarios/nuevouser","/usuarios/guardar","/usuarios/nuevoempresa","/usuario/**").permitAll().anyRequest()
+		http.authorizeRequests().antMatchers("/","/css/**", "/js/**", "/images/**","/usuarios/nuevouser","/usuarios/guardar","/usuarios/nuevoempresa","/master/master","/usuario/**").permitAll().anyRequest()
 				.authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll().and()
 				.exceptionHandling().accessDeniedPage("/error");
 	}
